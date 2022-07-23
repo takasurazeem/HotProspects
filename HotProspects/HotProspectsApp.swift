c82ef4537df69a9c072e950e647cbf1b67427b81
Takasur Azeem <takasurazeem@gmail.com>
2022-07-23 22:45:56 +0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Takasur Azeem on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
