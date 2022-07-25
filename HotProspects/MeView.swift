ebaab6c501e0f14ef6b9f80bf840eed0b0a09466
Takasur Azeem <takasurazeem@gmail.com>
2022-07-25 20:21:03 +0500

    Generating and scaling up a QR code
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -6,15 +6,58 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct MeView: View {
+    
+    @State private var name = "Anonymous"
+    @State private var emailAddress = "you@yoursite.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text("Salam!")
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                    .disableAutocorrection(true)
+                
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .textCase(.lowercase)
+                    .autocapitalization(.none)
+                    .textInputAutocapitalization(.never)
+                    .font(.title)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            .disableAutocorrection(true)
+            .navigationTitle("Your code")
+        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
 struct MeView_Previews: PreviewProvider {
     static var previews: some View {
         MeView()
+            .preferredColorScheme(.dark)
     }
 }
