5865e76506b71c75548ee91474a55fd6f91332ce
Takasur Azeem <takasurazeem@gmail.com>
2022-07-27 00:35:50 +0500

    challenge 3: Use a confirmation dialog to customize the way users are sorted in each tab – by name or by most recent.
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -15,6 +15,11 @@ struct ProspectsView: View {
         case none, contacted, uncontacted
     }
     
+    struct SortChoice: Hashable {
+        let name: String
+        let icon: String
+    }
+    
     let filter: FilterType
     
     var title: String {
@@ -28,19 +33,19 @@ struct ProspectsView: View {
         }
     }
     
-    var filteredProspects: [Prospect] {
-        switch filter {
-        case .none:
-            return prospects.people
-        case .contacted:
-            return prospects.people.filter { $0.isContacted }
-        case .uncontacted:
-            return prospects.people.filter { !$0.isContacted }
-        }
-    }
+    @State private var filteredProspects: [Prospect] = []
     
     @EnvironmentObject var prospects: Prospects
+    
     @State private var isShowingScanner = false
+    
+    let sortByChoices = [SortChoice(name: "Name", icon: "textformat.abc"), SortChoice(name: "Date Added", icon: "calendar.badge.clock")]
+    @State private var selection: SortChoice
+    
+    init(filter: FilterType) {
+        self.filter = filter
+        _selection = State(wrappedValue: sortByChoices.first!)
+    }
     
     var body: some View {
         NavigationView {
@@ -88,17 +93,51 @@ struct ProspectsView: View {
                     }
                 }
             }
+            .onAppear(perform: updatePerspectives)
             .navigationTitle(title)
             .toolbar {
-                Button {
-                    isShowingScanner = true
-                } label: {
-                    Label("Scan", systemImage: "qrcode.viewfinder")
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        isShowingScanner = true
+                    } label: {
+                        Label("Scan", systemImage: "qrcode.viewfinder")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu {
+                        Text("Sorted by")
+                        Picker("Sorted by", selection: $selection) {
+                            ForEach(sortByChoices, id: \.self) { choice in
+                                Label("\(choice.name)", systemImage: "\(choice.icon)")
+                            }
+                        }.onChange(of: selection, perform: sortPerspectives(newSelection:))
+                    } label: {
+                        Label("Filter", systemImage: "arrow.up.arrow.down.square")
+                    }
                 }
             }
             .sheet(isPresented: $isShowingScanner) {
                 CodeScannerView(codeTypes: [.qr], simulatedData: "Takasur Azeem\ntakasurazeem@gmail.com", completion: handleScan(result:))
             }
+        }
+    }
+
+    private func updatePerspectives() {
+        switch filter {
+        case .none:
+            filteredProspects = prospects.people
+        case .contacted:
+            filteredProspects = prospects.people.filter { $0.isContacted }
+        case .uncontacted:
+            filteredProspects = prospects.people.filter { !$0.isContacted }
+        }
+    }
+
+    func sortPerspectives(newSelection: SortChoice) {
+        if newSelection == sortByChoices.first {
+            filteredProspects = filteredProspects.sorted { $0.name < $1.name }
+        } else {
+            filteredProspects =  filteredProspects.sorted { $0.dateAdded < $1.dateAdded }
         }
     }
     
@@ -146,6 +185,7 @@ struct ProspectsView: View {
             person.emailAddress = details[1]
             
             prospects.add(person)
+            updatePerspectives()
         case .failure(let error):
             print("Scanning failed: \(error.localizedDescription)")
         }
@@ -157,5 +197,6 @@ struct ProspectsView_Previews: PreviewProvider {
     static var previews: some View {
         ProspectsView(filter: .none)
             .environmentObject(Prospects())
+            .preferredColorScheme(.dark)
     }
 }
