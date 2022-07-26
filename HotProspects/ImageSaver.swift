43c5c0ae546cae1c503e369d2a00fcf0eace0b58
Takasur Azeem <takasurazeem@gmail.com>
2022-07-26 15:04:32 +0500

    Adding a context menu to an image
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

---
+++ HotProspects/ImageSaver.swift
@@ -0,0 +1,27 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Takasur Azeem on 05/07/2022.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @objc private func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+    
+}
--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -12,6 +12,7 @@ struct MeView: View {
     
     @State private var name = "Anonymous"
     @State private var emailAddress = "you@yoursite.com"
+    @State private var qrCode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -31,14 +32,25 @@ struct MeView: View {
                     .textInputAutocapitalization(.never)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        Button {
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrCode)
+                        } label: {
+                            Label("Save to Photos", systemImage: "square.and.arrow.down")
+                        }
+                    }
             }
-            .disableAutocorrection(true)
+//            .disableAutocorrection(true)
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name) { _ in updateCode() }
+            .onChange(of: emailAddress) { _ in updateCode() }
         }
     }
     
@@ -52,6 +64,10 @@ struct MeView: View {
         }
         
         return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
 }
 
